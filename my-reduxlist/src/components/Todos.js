1ac40c370afb2dd5cb3b5fd5ecde596a45db4f3e
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-07-28 12:38:27 +0300

    wip-component-todos

--- my-reduxlist/src/components/Todos.js
+++ my-reduxlist/src/components/Todos.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { connect } from "react-redux";
 import { addTodos, removeTodos } from "../redux/reducer";
 
@@ -17,6 +17,13 @@ const mapDispatchToProps = (dispatch) => {
 
 const Todos = (props) => {
   const [todo, setTodo] = useState("");
+
+  const inputRef = useRef(true);
+
+  const changeFocus = () => {
+      inputRef.current.disabled = false;
+      inputRef.current.focus();
+  }
 
   const handleChange = (e) => {
     setTodo(e.target.value);
@@ -48,7 +55,14 @@ const Todos = (props) => {
         {props.todos.map((item) => {
           return (
             <li key={item.id}>
-              {item.item} <button onClick={()=>props.removeTodo(item.id)}>Delete</button>{""}
+              <textarea
+                ref={inputRef}
+                disabled={inputRef}
+                defaultValue={item.item}
+              />
+              <button onClick={()=>changeFocus()}>Edit</button>
+              <button onClick={() => props.removeTodo(item.id)}>Delete</button>
+              {""}
             </li>
           );
         })}
