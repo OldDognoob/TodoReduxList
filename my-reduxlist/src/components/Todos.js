df2820f4a0e5ca5824ebf27f8ed61f2a82c5dd7c
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-07-28 17:41:11 +0300

    wip-component-todos

--- my-reduxlist/src/components/Todos.js
+++ my-reduxlist/src/components/Todos.js
@@ -1,11 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import {
-  addTodos,
-  removeTodos,
-  updateTodos,
-  completeTodos,
-} from "../redux/reducer";
+import { addTodos } from "../redux/reducer";
+import { GoPlus } from "react-icons/go";
+import { motion } from "framer-motion";
 
 const mapStateToProps = (state) => {
   return {
@@ -16,9 +13,6 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     addTodo: (obj) => dispatch(addTodos(obj)),
-    removeTodo: (id) => dispatch(removeTodos(id)),
-    updateTodo: (obj) => dispatch(updateTodos(obj)),
-    completeTodo: (id) => dispatch(completeTodos(id)),
   };
 };
 
@@ -28,30 +22,40 @@ const Todos = (props) => {
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
-  console.log("props from store", props);
 
+  const add = () => {
+    if (todo === "") {
+      alert("Input is Empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
+    }
+  };
+  //console.log("props from store", props);
   return (
     <div className="addTodos">
       <input
         type="text"
         onChange={(e) => handleChange(e)}
         className="todo-input"
+        value={todo}
       />
-      <button
+
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
         className="add-btn"
-        onClick={() =>
-          props.addTodo({
-            id: Math.floor(Math.random() * 1000),
-            item: todo,
-            completed: false,
-          })
-        }
+        onClick={() => add()}
       >
-        Add
-      </button>
+        <GoPlus />
+      </motion.button>
       <br />
     </div>
   );
 };
-
+//we can use connect method to connect this component with redux store
 export default connect(mapStateToProps, mapDispatchToProps)(Todos);
